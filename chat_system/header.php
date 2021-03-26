4dafb66c9126d73354dbc3b1d6bf8c101302951a
mathura15 <65862666+mathura15@users.noreply.github.com>
2021-03-26 09:50:18 +0530

    initial commit

---
+++ chat_system/header.php
@@ -0,0 +1,17 @@
+<div id="header">
+    <div id="logo">
+        <a href="index1.php?home"><img src="../images/white.png" style="width:60%;" alt=""/></a>
+    </div>
+    <div id="name">
+    <h1>CHAT PART</h1>
+    </div>
+
+    <div id="link">
+    <ul>
+        <li>
+            <a href="../logout.php"><b>Log out</b></a>
+        </li>
+    </ul>
+    </div>
+
+</div>
